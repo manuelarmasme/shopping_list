1ae139f288da9fe93dcea2151879a5100fce2652
manuelarmasme <manu12694@gmail.com>
2024-01-20 17:55:31 -0400

    ready for adding firebase backend

---
+++ lib/widgets/new_item.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/models/category.dart';
+import 'package:shopping_list/models/grocery_item.dart';
+
+class NewItem extends StatefulWidget {
+  const NewItem({super.key});
+
+  @override
+  State<NewItem> createState() {
+    return _NewItemState();
+  }
+}
+
+class _NewItemState extends State<NewItem> {
+  //form key
+  final _formKey = GlobalKey<FormState>();
+
+  //forms values
+  var _enteredName = '';
+  var _enteredQuantity = 1;
+
+  //this var hels to have an initial value into the dropdown
+  var _selectedCategory = categories[Categories.vegetables]!;
+
+  //function to save an item
+  void _saveItem() {
+    //! this tells flutter that it won't be null
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      Navigator.of(context).pop(
+        GroceryItem(
+          id: DateTime.now().toString(),
+          name: _enteredName,
+          quantity: _enteredQuantity,
+          category: _selectedCategory,
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Create a new Item'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: const InputDecoration(label: Text('name')),
+                validator: (value) {
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.trim().length <= 1 ||
+                      value.trim().length > 50) {
+                    return 'Must be between 1 and 50 characteres';
+                  }
+
+                  return null;
+                },
+                onSaved: (value) {
+                  // ! telling flutter it won't be null
+                  _enteredName = value!;
+                },
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration:
+                          const InputDecoration(label: Text('quantity')),
+                      keyboardType: TextInputType.number,
+                      initialValue: _enteredQuantity.toString(),
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) == null ||
+                            //we are using ! to tell dart that in this case the value is not null
+                            //because if it null the upper validation will be active
+                            int.tryParse(value)! <= 0) {
+                          return 'Must be a positive number';
+                        }
+
+                        return null;
+                      },
+                      onSaved: (value) {
+                        //we use parse because it shows error and not null like tryparse
+                        _enteredQuantity = int.parse(value!);
+                      },
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  //We have to wrap into an expanded because it cause problem with the row
+                  Expanded(
+                    child: DropdownButtonFormField(
+                        value: _selectedCategory,
+                        items: [
+                          for (final category in categories.entries)
+                            DropdownMenuItem(
+                              value: category.value,
+                              child: Row(
+                                children: [
+                                  Container(
+                                    height: 16,
+                                    width: 16,
+                                    color: category.value.color,
+                                  ),
+                                  const SizedBox(width: 6),
+                                  Text(category.value.title)
+                                ],
+                              ),
+                            )
+                        ],
+                        onChanged: (value) {
+                          //we use set state because depending on the selected option we have to update de UI
+                          setState(() {
+                            _selectedCategory = value!;
+                          });
+                        }),
+                  )
+                ],
+              ),
+              const SizedBox(height: 12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      _formKey.currentState!.reset();
+                    },
+                    child: const Text('Reset'),
+                  ),
+                  ElevatedButton(
+                    onPressed: _saveItem,
+                    child: const Text('Add Item'),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
